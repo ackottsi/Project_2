137344f41e5fdcf82ce356ac32f8534d91ccc424
ackottsi <kottsickandrewc@johndeere.com>
2021-02-01 16:36:36 -0600

    verify server is running

---
+++ Movie-app/server.js
@@ -0,0 +1,7 @@
+const express = require('express'); //from documentation: express is function
+const app = express();//app is an object
+
+app.listen(3000, ()=>{
+    console.log("I am listening");
+});
+
