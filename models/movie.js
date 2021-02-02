8bcdd4a0d5727beafa0119e7a8f698c227945e59
bruce-h <bdhay@protonmail.com>
2021-02-02 14:05:21 -0600

    Movies db created and seeded

--- models/movie.js
+++ models/movie.js
@@ -7,17 +7,15 @@ module.exports = (sequelize, DataTypes) => {
     /**
      * Helper method for defining associations.
      * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automaticall
+     * The `models/index` file will call this method automatically.
      */
     static associate(models) {
       // define association here
-      Movie.hasMany(models.Actor, { foreignKey: "movieId" });
     }
   };
   Movie.init({
     name: DataTypes.STRING,
     dateReleased: DataTypes.DATE
-
   }, {
     sequelize,
     modelName: 'Movie',
